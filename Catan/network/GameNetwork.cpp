86517e2343fd03b58db9f2b692f90bd5104cfe5e
jov580 <jovanalazic580@gmail.com>
2026-01-24 16:43:13 +0100

    feat(network-cllient): implemented class GameNetwork and fixed bugs

--- Catan/network/GameNetwork.cpp
+++ Catan/network/GameNetwork.cpp
@@ -1,3 +1,42 @@
 //
 // Created by jovan on 24/01/2026.
 //
+
+#include "GameNetwork.h"
+#include "networkmove/MoveProtoSerializer.h"
+#include "networkmove/MoveProtoFactory.h"
+
+GameNetwork::GameNetwork(QObject* parent)
+  : QObject(parent)
+{
+    connect(&transport, &NetworkTransport::envelopeReceived,
+            this, &GameNetwork::onEnvelope);
+}
+
+void GameNetwork::connectToServer(const QHostAddress& addr, uint16_t port) {
+    transport.connectTo(addr, port);
+}
+
+net::Envelope GameNetwork::wrapMove(const Move& move) {
+    net::Envelope env;
+    env.set_msg_type(net::MSG_MOVE);
+    env.set_seq(nextSeq++);
+
+    *env.mutable_move() = MoveProtoSerializer::toProto(move);
+
+    return env;
+}
+
+void GameNetwork::sendMove(const Move& move) {
+    net::Envelope env = wrapMove(move);
+    transport.sendEnvelope(env);
+}
+
+void GameNetwork::onEnvelope(const net::Envelope& env) {
+    if (env.msg_type() != net::MSG_MOVE)
+        return;
+
+    auto move = MoveProtoFactory::fromProto(env.move());
+    if (move)
+        emit remoteMoveReceived(std::move(move));
+}
